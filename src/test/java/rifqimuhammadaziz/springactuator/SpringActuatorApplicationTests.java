c0385bcf212bde4a7fedc62ae67e1362d97e8701
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-05-23 09:37:11 +0700

    Create spring project

---
+++ src/test/java/rifqimuhammadaziz/springactuator/SpringActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.springactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
